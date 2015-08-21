d0a7005d3c45ef5c73c0bc10b27cd6d32c8e0db8
Mark Piper <mark.piper@colorado.edu>
2015-08-21 11:06:54 -0600

    Import component with name 'Component'

--- tests/test_satzonedarcylayers_cmi.py
+++ tests/test_satzonedarcylayers_cmi.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 import os
-from cmt.components import SatZoneDarcyLayers
+from cmt.components import SatZoneDarcyLayers as Component
 from . import example_dir
 
 
 cfg_file = os.path.join(example_dir, 'June_20_67_satzone_darcy_layers.cfg')
 
 def test_irf():
-    component = SatZoneDarcyLayers()
+    component = Component()
     component.initialize(cfg_file)
     component.update(1.0)
     component.finalize()
