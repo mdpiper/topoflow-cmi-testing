5195c41491649268f54f40f4aac07c8f5d25b821
Mark Piper <mark.piper@colorado.edu>
2015-08-21 11:16:19 -0600

    Add tests for Meteorology and GC2D

---
+++ tests/test_ice_gc2d.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+from cmt.components import GC2D as Component
+from . import example_dir
+
+
+cfg_file = os.path.join(example_dir, 'June_20_67_ice_valley_glacier.cfg')
+
+def test_irf():
+    component = Component()
+    component.initialize(cfg_file)
+    component.update(1.0)
+    component.finalize()
