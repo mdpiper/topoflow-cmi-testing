73276a31bd750c06171dd365e0e63ccbe269b450
Mark Piper <mark.piper@colorado.edu>
2015-08-21 11:13:22 -0600

    Add tests for snow components

---
+++ tests/test_snow_degree_day.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+from cmt.components import SnowDegreeDay as Component
+from . import example_dir
+
+
+cfg_file = os.path.join(example_dir, 'June_20_67_snow_degree_day.cfg')
+
+def test_irf():
+    component = Component()
+    component.initialize(cfg_file)
+    component.update(1.0)
+    component.finalize()
