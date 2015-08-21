14accf95f32d0e427410b96de6e424645b9454aa
Mark Piper <mark.piper@colorado.edu>
2015-08-21 11:01:43 -0600

    Relocate locate_topoflow function

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,18 @@
+"""Tests for running TopoFlow components in CMI."""
 import os
+
+
+def locate_topoflow(cache_dir):
+    for x in os.listdir(cache_dir):
+        if x.startswith('topoflow'):
+            return x
 
 
 root_dir = '/home/csdms/wmt/topoflow.1'
 cache_dir = os.path.join(root_dir, 'cache')
-topoflow_cache_dir = locate_topoflow_cache_dir()
-example_dir = os.path.join(cache_dir, topoflow_cache_dir,
+topoflow_dir = locate_topoflow(cache_dir)
+example_dir = os.path.join(cache_dir, topoflow_dir,
                            'topoflow', 'examples', 'Treynor_Iowa')
 
 
-def locate_topoflow_cache_dir():
-    for x in os.listdir(cache_dir):
-        if x.startswith('topoflow'):
-            return x
 
